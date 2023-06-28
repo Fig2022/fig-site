676986b4896e8b81de59e94ebb41f58dffbfbb34
fig <arielfishkin@gmail.com>
2023-06-28 16:04:57 -0700

    commented out the hammer and music note SVG to troubleshoot error

--- src/pages/index.js
+++ src/pages/index.js
@@ -41,19 +41,18 @@ const Homepage = () => {
 
             <li class="flex items-center">
 
-
-              <svg class="flex-none fill-blue-400 bi bi-music-note" xmlns="http://www.w3.org/2000/svg" width="35" height="35" viewBox="0 0 16 16">
+              {/* <svg class="flex-none fill-blue-400 bi bi-music-note" xmlns="http://www.w3.org/2000/svg" width="35" height="35" viewBox="0 0 16 16">
               <path d="M9 13c0 1.105-1.12 2-2.5 2S4 14.105 4 13s1.12-2 2.5-2 2.5.895 2.5 2z" stroke="currentColor" stroke-width="0.25"/>
               <path fill-rule="evenodd" d="M9 3v10H8V3h1z" stroke="currentColor" stroke-width="0.25"/>
               <path d="M8 2.82a1 1 0 0 1 .804-.98l3-.6A1 1 0 0 1 13 2.22V4L8 5V2.82z" stroke="currentColor" stroke-width="0.25"/>
-              </svg>
+              </svg> */}
 
             <li class="flex items-center">
-              <svg class="flex-none fill-violet-300 bi bi-music-note" xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 16 16">
+              {/* <svg class="flex-none fill-violet-300 bi bi-music-note" xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 16 16">
               <path d="M9 13c0 1.105-1.12 2-2.5 2S4 14.105 4 13s1.12-2 2.5-2 2.5.895 2.5 2z"/>
               <path fill-rule="evenodd" d="M9 3v10H8V3h1z"/>
               <path d="M8 2.82a1 1 0 0 1 .804-.98l3-.6A1 1 0 0 1 13 2.22V4L8 5V2.82z"/>
-              </svg>
+              </svg> */}
 
               <p class="ml-4 font-mono tracking-wide">
                 <a href="https://bloodhunny.bandcamp.com/">Making Music Things</a>
